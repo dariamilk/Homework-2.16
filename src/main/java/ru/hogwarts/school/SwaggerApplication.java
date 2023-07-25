c8a6a397aede2eac7fe6387a772de8ee8c5a3e44
dariamilk <daryachulpanova@gmail.com>
2023-07-25 22:53:22 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/SwaggerApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerApplication.class, args);
+    }
+
+}
